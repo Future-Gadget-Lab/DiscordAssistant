aadd7987d590932b245f0fcd4a133d592e75533b
xoltia <38849891+xoltia@users.noreply.github.com>
2019-11-10 17:13:20 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assistant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
