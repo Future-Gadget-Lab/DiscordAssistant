7ce75ded1e08611b4436ebf45d7700b9595f1e9d
xoltia <38849891+xoltia@users.noreply.github.com>
2019-11-12 09:28:16 -0600

    Add asp.net to docs module

--- src/Modules/MicrosoftDocs/DocsModule.cs
+++ src/Modules/MicrosoftDocs/DocsModule.cs
@@ -18,8 +18,9 @@ namespace Assistant.Modules.MicrosoftDocs
 
         private static readonly Dictionary<string, Color> ScopeThemes = new Dictionary<string, Color>
         {
-            { ".NET", new Color(0x512bd4) },
-            { "SQL",  new Color(0x243a5e) },
+            { ".NET",    new Color(0x512bd4) },
+            { "ASP.NET", new Color(0x512bd4) },
+            { "SQL",     new Color(0x243a5e) },
         };
 
         public DocsModule(HttpService http)
@@ -49,6 +50,10 @@ namespace Assistant.Modules.MicrosoftDocs
         [Command("dotnet"), Alias(".net")]
         public async Task DotnetDocs([Remainder]string search) =>
             await ReplyAsync(embed: await GetDocsEmbed(search, ".NET"));
+
+        [Command("asp"), Alias("asp.net")]
+        public async Task AspDocs([Remainder]string search) =>
+            await ReplyAsync(embed: await GetDocsEmbed(search, "ASP.NET"));
 
         [Command("sql")]
         public async Task SqlDocs([Remainder]string search) =>
