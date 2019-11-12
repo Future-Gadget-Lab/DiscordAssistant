2854fb870b519b16bede35b5938579dc7790ce60
xoltia <38849891+xoltia@users.noreply.github.com>
2019-11-12 09:17:25 -0600

    Fix naming convention inconsistency

--- src/Modules/MicrosoftDocs/DocsModule.cs
+++ src/Modules/MicrosoftDocs/DocsModule.cs
@@ -12,8 +12,8 @@ namespace Assistant.Modules.MicrosoftDocs
         // TODO: allow searching by category (reference/learn/docs/all)
 
         private readonly HttpService _http;
-        private static readonly string API_BASE = "https://docs.microsoft.com/api/search?search={0}&locale=en-us&%24filter=scopes%2Fany%28t%3A+t+eq+%27{1}%27%29&facet=category&%24skip=0&%24top=5";
-        private static readonly string DOCS_BASE = "https://docs.microsoft.com/en-us/search/?search={0}&category=All&scope={1}";
+        private static readonly string ApiBase = "https://docs.microsoft.com/api/search?search={0}&locale=en-us&%24filter=scopes%2Fany%28t%3A+t+eq+%27{1}%27%29&facet=category&%24skip=0&%24top=5";
+        private static readonly string DocsBase = "https://docs.microsoft.com/en-us/search/?search={0}&category=All&scope={1}";
 
         public DocsModule(HttpService http)
         {
@@ -22,11 +22,11 @@ namespace Assistant.Modules.MicrosoftDocs
 
         private async Task<Embed> GetDocsEmbed(string search, string scope)
         {
-            Docs docs = await _http.GetModel<Docs>(HttpService.FormatUrl(API_BASE, search, scope));
+            Docs docs = await _http.GetModel<Docs>(HttpService.FormatUrl(ApiBase, search, scope));
             EmbedBuilder embed = new EmbedBuilder()
                 .WithTitle(search)
                 .WithAuthor("Microsoft Docs", "https://docs.microsoft.com/en-us/media/logos/logo-ms-social.png", "https://docs.microsoft.com")
-                .WithDescription($"[View on website]({HttpService.FormatUrl(DOCS_BASE, search, scope)})")
+                .WithDescription($"[View on website]({HttpService.FormatUrl(DocsBase, search, scope)})")
                 .WithFields(docs.Results.Select(r =>
                     new EmbedFieldBuilder().WithName(r.Title).WithValue($"[{r.DisplayUrl.Content}]({r.Url})\n{r.Description}")
                 ))
